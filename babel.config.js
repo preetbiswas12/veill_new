b2517e635a97e70c39c1f5ab957e4155d4e3977e
preetbiswas12 <preetbiswas2006@gmail.com>
2026-08-18 16:33:48 +0530

    Use babel-plugin-transform-define to inline EXPO_ROUTER_APP_ROOT for Metro bundling

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      ['babel-plugin-transform-define', {
+        'process.env.EXPO_ROUTER_APP_ROOT': JSON.stringify('app')
+      }]
+    ],
   };
 };
