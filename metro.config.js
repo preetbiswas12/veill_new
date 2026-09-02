cd153c9d49c774ac8c086bbdf53f553c80f1bf61
preetbiswas12 <preetbiswas2006@gmail.com>
2026-09-02 22:58:35 +0530

    fix: remove dead expo-crypto stub reference from metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -4,9 +4,4 @@ const config = getDefaultConfig(__dirname);
 
 config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs'];
 
-config.resolver.extraNodeModules = {
-  ...(config.resolver.extraNodeModules || {}),
-  'expo-crypto': require.resolve('./stubs/expo-crypto-stub.js'),
-};
-
 module.exports = config;
