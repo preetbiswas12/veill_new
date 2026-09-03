39dce129ec8f0ac419936cf4c57f12497de724d9
preetbiswas12 <preetbiswas2006@gmail.com>
2026-09-03 12:19:07 +0530

    fix: configure @ path alias in Metro and tsconfig
    - Add paths to tsconfig.json for @/* and @utils/*
    - Add extraNodeModules to metro.config.js to resolve @ and @utils
    - Fixes: Unable to resolve module @/utils/daily

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,10 @@ const config = getDefaultConfig(__dirname);
 
 config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs'];
 
+config.resolver.extraNodeModules = {
+  ...(config.resolver.extraNodeModules || {}),
+  '@': require('path').resolve(__dirname, 'src'),
+  '@utils': require('path').resolve(__dirname, 'utils'),
+};
+
 module.exports = config;
