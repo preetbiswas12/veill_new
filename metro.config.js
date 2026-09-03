d3eab0a53395421854e45355f0cd973818f68d2a
preetbiswas12 <preetbiswas2006@gmail.com>
2026-09-03 12:35:12 +0530

    fix: use relative import for daily.ts in CallDetails
    @/utils/daily path alias not resolving in Metro bundler
    Changed to relative import ../../utils/daily

--- metro.config.js
+++ metro.config.js
@@ -4,10 +4,4 @@ const config = getDefaultConfig(__dirname);
 
 config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs'];
 
-config.resolver.extraNodeModules = {
-  ...(config.resolver.extraNodeModules || {}),
-  '@': require('path').resolve(__dirname, 'src'),
-  '@utils': require('path').resolve(__dirname, 'utils'),
-};
-
 module.exports = config;
